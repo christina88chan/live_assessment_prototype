d55eaaaac9b7803e8a06f5a87611b1922b476562
Aashna Shimkhada <55328807+AashnaShimkhada@users.noreply.github.com>
2025-08-14 04:15:26 +0000

    added pop up confirming logout and asking user to redirect back to student view

--- ui_shared.py
+++ ui_shared.py
@@ -13,10 +13,18 @@ def admin_sidebar():
             st.switch_page("pages/admin_dash.py")
         st.divider()
         if st.button("🎧 Student View", use_container_width=True, key="nav_student"):
-            st.switch_page("student.py")
+            st.switch_page("student_login_.py")
         if st.button("Logout", use_container_width=True, key="nav_logout"):
-            st.switch_page("student.py")
+            # Show confirmation popup
+            st.toast("You have successfully logged out. Please redirect to Student View", icon="✅")
+            # Optional: Pause briefly so user sees the toast
+            # import time
+            # time.sleep(2)
+            # Redirect to student login
+            # Clear session safely
             sign_out()
+            # st.switch_page("student_login_.py")
+
 
 def student_top_button():
     col1, col2, col3 = st.columns([1, 1, 1])
